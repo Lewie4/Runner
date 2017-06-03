4edad3a1d27433954b1ab9b8a1624873f104c05d
Matilde Herland <matildeherland@gmail.com>
2017-06-03 18:07:35 +0100

    Added first level, pug animations

---
+++ Unity/Runner/Assets/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private GameObject m_pug;
+
+    private void Update()
+    {
+        transform.position = new Vector3(m_pug.transform.position.x, m_pug.transform.position.y, m_pug.transform.position.z - 15);
+    }
+}
